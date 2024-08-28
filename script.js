ae89846614c309f69e05f61795832d2c1502bb8f
regis066 <mugisharegis72@gmail.com>
2024-08-28 14:37:55 +0200

    Add collision detection for the ball to bounce back

--- script.js
+++ script.js
@@ -7,9 +7,11 @@ let y = canvas.height - 30; //190
 let dx = 2;
 let dy = -2;
 
+const ballRadius = 10;
+
 function drawBall() {
   ctx.beginPath();
-  ctx.arc(x, y, 10, 0, Math.PI * 2);
+  ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
   ctx.fillStyle = "#0095DD";
   ctx.fill();
   ctx.closePath();
@@ -20,13 +22,21 @@ function draw() {
   drawBall();
   x += dx;
   y += dy;
+
+  if (x + dx < ballRadius || x + dx > canvas.width - ballRadius) {
+    dx = -dx;
+  }
+
+  if (y + dy < ballRadius || y + dy > canvas.height - ballRadius) {
+    dy = -dy;
+  }
 }
 
 function startGame() {
-    setInterval(draw, 10);
+  setInterval(draw, 10);
 }
 
-document.getElementById("runButton").addEventListener('click', function () {
-    startGame();
-    this.disabled = true;
-})
+document.getElementById("runButton").addEventListener("click", function () {
+  startGame();
+  this.disabled = true;
+});
